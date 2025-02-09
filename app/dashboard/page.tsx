4f5069f4dac85a2cd4e7426dddbb8e0f09c08fb9
Natan Gamliel <ngaml@Natans-MacBook-Pro.local>
2025-02-09 10:08:44 -0500

    set up basic nav

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      
+    </>
+  );
+}
---
+++ app/dashboard/tournament/page.tsx
@@ -0,0 +1,38 @@
+import { Team } from '@/app/lib/definitions';
+
+export default async function Page() {
+  const getTeamData = async () => {
+    const res = await fetch(`${process.env.MMSCORES_BASE_URL}/teams`);
+    const data = await res.json();
+    return data;
+  };
+
+  const data = await getTeamData();
+  console.log(data);
+
+  return (
+    <>
+      <h1>Tournament Page</h1>
+      <table className="w-full">
+        <thead>
+          <tr>
+            <td>Team</td>
+            <td>Seed</td>
+            <td>Total Points</td>
+            <td>Status</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((team:Team) => (
+            <tr key={team.id}>
+              <td>{team.name}</td>
+              <td>{team.seed}</td>
+              <td>{team.seed * 15 + team.confirmedpts + team.inprogresspts}</td>
+              <td>{team.intournament ? "" : "Eliminated"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
