7547a3a120cf994a9905dab2ed3a140d4ca50746
nsgamliel <nsgamliel@gmail.com>
2025-03-01 12:38:04 -0500

    add albanian flag in header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,16 +1,33 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 import Link from 'next/link';
 import { lusitana } from '../ui/fonts';
+import Image from 'next/image';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Link
-        className="flex h-[10vh] items-center justify-center bg-red-700 p-4 md:h-[10vh] fixed left-0 right-0 top-0"
+        className="flex h-[10vh] items-center justify-center bg-red-700 md:h-[10vh] md:p-2 fixed left-0 right-0 top-0"
         href="/"
       >
-        <div className="flex items-center w-full text-white">
-          <h1 className={`${lusitana.className} text-md md:text-3xl text-white w-full text-center font-bold`}>Albanian March Madness Contest of Champions</h1>
+        <div className="flex items-center w-full text-white h-full">
+          <Image 
+            src="/albania.png"
+            alt="Albanian Flag"
+            width={400}
+            height={400}
+            style={{objectFit: "contain", maxHeight: "100%" }}
+            className='w-1/2 h-auto max-h-none md:max-h-full'
+          />
+          <h1 className={`${lusitana.className} text-lg md:text-3xl text-white w-full text-center font-bold p-4`}>Albanian March Madness Contest of Champions</h1>
+          <Image 
+            src="/albania.png"
+            alt="Albanian Flag"
+            width={400}
+            height={400}
+            style={{objectFit: "contain", maxHeight: "100%" }}
+            className='w-1/2 h-auto max-h-none md:max-h-full hidden md:block'
+          />
         </div>
       </Link>
       <div className="flex h-[90vh] mt-[10vh] flex-col md:flex-row md:overflow-hidden">
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <>
       <main className="flex min-h-screen flex-col p-0">
-        <div className="flex h-32 fixed left-0 right-0 top-0 shrink-0 items-center bg-red-700 p-4 md:h-48">
-          <h1 className={`${lusitana.className} text-3xl md:text-6xl text-white w-full text-center font-bold`}>Albanian March Madness Contest of Champions</h1>
+        <div className="flex h-36 fixed left-0 right-0 top-0 shrink-0 items-center bg-red-700 p-4 md:h-48">
+          <Image 
+            src="/albania.png"
+            alt="Albanian Flag"
+            width={400}
+            height={400}
+            style={{objectFit: "contain", maxHeight: "100%" }}
+            className='w-1/2 h-auto max-h-none md:max-h-full'
+          />
+          <h1 className={`${lusitana.className} text-2xl md:text-5xl text-white w-full text-center font-bold p-2`}>Albanian March Madness Contest of Champions</h1>
+          <Image 
+            src="/albania.png"
+            alt="Albanian Flag"
+            width={400}
+            height={400}
+            style={{objectFit: "contain", maxHeight: "100%" }}
+            className='hidden md:block'
+          />
         </div>
-        <div className="m-4 mt-36 md:mt-52 flex grow flex-col gap-4 md:flex-row">
-          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-100 px-6 py-10 w-full md:w-2/5 md:px-10">
+        <div className="m-4 mt-40 md:mt-52 flex grow flex-col gap-4 md:flex-row">
+          <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-100 px-6 py-10 w-full md:w-1/2 md:px-10">
             <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-              <strong>Welcome to Albanian March Madness.</strong>
+              <strong>Welcome to the Albanian March Madness Contest of Champions.</strong>
             </p>
             <p>If that sounds familiar, you're in the right place. Otherwise, don't worry about it.</p>
             <p>Be a mensch.</p>
@@ -20,7 +37,7 @@ export default function Page() {
               href="/dashboard/leaderboard"
               className="flex items-center gap-5 self-start rounded-lg bg-red-700 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-500 md:text-base"
             >
-              <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+              <span>View Tournament</span> <ArrowRightIcon className="w-5 md:w-6" />
             </Link>
           </div>
           <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
